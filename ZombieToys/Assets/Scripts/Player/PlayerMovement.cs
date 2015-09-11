fc8784a682e5b19c99cfb522316161a85638cafa
Simon LaRoche <slaroche@IT000.mr.preble.com>
2015-09-11 12:14:21 -0400

    Part 2 Completed!

--- ZombieToys/Assets/Scripts/Player/PlayerMovement.cs
+++ ZombieToys/Assets/Scripts/Player/PlayerMovement.cs
@@ -19,9 +19,14 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void FixedUpdate()
-    {
+    { 
+
         float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        float v = Input.GetAxisRaw("vertical");
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
 
     }
 
@@ -34,5 +39,27 @@ public class PlayerMovement : MonoBehaviour
         playerRigidbody.MovePosition(transform.position + movement);
     }
 
-    
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
+
+
+            
 }
